9a3bcb551795de8ab4a8cd67819fcbf5532b4730
Paul Brown <paul90brown@gmail.com>
2020-05-01 21:48:53 -0500

    add DATABASES to tests/settings.py

--- tests/settings.py
+++ tests/settings.py
@@ -1,2 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
 SECRET_KEY = ')dajq1#olz2*y&$1x0y&pd0ev-a_h2*j%ed0j5ych2^oy%*2%e'
 DATETIME_INPUT_FORMATS = ('%Y-%m-%dT%H:%M:%S%z',)
